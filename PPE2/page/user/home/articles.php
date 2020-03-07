4e3c92bd1d5b25c0261e65fe5df50dbed4ea705d
amaurydelassus <amaury.delassus@gmail.com>
2020-03-07 17:13:52 +0100

    #06 Debug-Edit articles.php

--- PPE2/page/user/home/articles.php
+++ PPE2/page/user/home/articles.php
@@ -16,6 +16,24 @@ if(isset($_SESSION["login"]) && $_SESSION['login'] != "")
                         <h1>Consomable</h1><br/><br/>
                         <div class="row" style = "width:100%">
                         <?php
+                        $c = new Article();
+                        $out = $c->getAllArticle($bdd);
+                        foreach($out as $val)
+                        {
+                            ?>
+                            <div class="col-md-3 Article" onclick='location.href="index.php?page=article&id=<?php echo $val["id"]?>"'>
+                                ------------------------------------ <br/>
+                                Image : <img src="<?php echo $val['Image'] ?>"/></a><br/>
+                                Ref : <?php echo $val['id']; ?><br/>
+                                Titre : <?php echo $val['Nom']; ?><br/>
+                                prix : <?php echo $val['Prix']; ?><br/>
+                                stoque :<?php echo $val['Stoque']; ?><br/>
+        
+                                ------------------------------------ <br>
+                            </div>
+                            <?php
+                        }
+
                     }
                     else
                     {
@@ -23,10 +41,23 @@ if(isset($_SESSION["login"]) && $_SESSION['login'] != "")
                         <h1>Borne</h1><br/><br/>
                         <div class="row" style = "width:100%">
                         <?php
+                        $c = new Borne();
+                        $out = $c->getAllBorne($bdd);
+                        foreach($out as $val){
+                            ?>
+                            <div class="col-md-3 Article" onclick='location.href="index.php?page=borne&id=<?php echo $val["id"]?>"'>
+                                ------------------------------------ <br/>
+                                Image : <img src="img/<?php echo $val['Image']?>"/></a><br/>
+                                Ref : <?php echo $val['id']?><br/>
+                                Titre : <?php echo $val['Nom']?><br/>
+                                Prix : <?php echo $val['Prix']?><br/>
+                                Description :<?php echo $val['Description']?><br/>
+        
+                                ------------------------------------ <br>
+                            </div>
+                            <?php
+                        }
                     }
-                    $type = $_POST['type'];
-                    $c = new Article();
-                    $c->getAllArticleByType($type,$bdd);
                 }
                 ?>
             </div>
